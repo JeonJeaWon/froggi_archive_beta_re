fef6ca96821103bc7efd55473840fb003d5c8972
JeonJeaWon <37402140+JeonJeaWon@users.noreply.github.com>
2021-05-11 21:13:48 +0900

    back

--- project_2020/settings.py
+++ project_2020/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
